54f9ebcd8b8b3e6c7c72000799996b92a34a4f8f
Jeahnoh <123100450+cdunlap-gravic@users.noreply.github.com>
2024-07-22 12:21:59 -0400

    Initial push with basic module setup. Didn't get it running from terminal on work pc, but might work from chromebook or MSI. - J

---
+++ modules/menuFromJson.py
@@ -0,0 +1,76 @@
+# Imports
+import json
+import tkinter as tk
+import sys
+
+# Global Variables
+optArg = {}
+
+#####################################################################################################
+# Functions: 
+#####################################################################################################
+
+def loadMenuFromJson(filePath, parentMenu):
+    """
+     Base function for loading a menu from a JSON file
+        
+     @param filePath - path to the JSON file
+     @param parentMenu - menu to adhere any commmands or sub menus
+    """
+    with open(filePath) as file:
+        data = json.load(file)
+        for key, value in data.items():
+            if key == "subMenu":
+                recLoadMenuFromJson(value, parentMenu)
+
+
+#######################################################################
+#######################################################################
+def recLoadMenuFromJson(dataList, parentMenu):
+    """
+     Recursive function for loading a menu from a JSON file to handle actual menu generation, and self terminate to root menu
+    
+    NOTE: This recursive function will need heavy rework and additions. Current logic may not be efficient and I like to remove any overhead if possible
+        Also, there are other menu generation options that are not currently supported, i.e. checked values etc.
+    
+    NOTE: Please see example JSON file provided for expected structure
+    
+     @param dataList - list object containing an array of commands and/or sub menus
+     @param parentMenu - menu to adhere any commmands or sub menus. Key object for recursive functionality
+    """
+    for item in dataList:
+        if isinstance(item, dict):
+            for key, value in item.items():
+                if key == "type":  
+                    dictType = value
+                elif key == "menuName":
+                    subMenu = tk.Menu(parentMenu, tearoff=0)
+                elif isinstance(value, list): 
+                    if dictType == "menu":
+                        parentMenu.add_cascade(**optArg, menu = subMenu)
+                        optArg.clear()
+                        recLoadMenuFromJson(value, subMenu)
+                elif key == (list(item)[-1]):
+                    if dictType == "command": 
+                        parentMenu.add_command(**optArg, command = eval(value))
+                        optArg.clear()
+                    elif dictType == "separator":
+                        parentMenu.add_separator()
+                        optArg.clear()
+                    elif dictType == "iterator":
+                        optArg.update({key: value})
+                        iters = optArg.get('iterations')
+                        optArg.pop('iterations')
+                        optArgCP = optArg.copy()
+                        for i in range(iters):
+                            for fixKey, fixValue in optArg.items():
+                                optArgCP.update({fixKey: f'{str(fixValue)}{i+1}'})
+                            parentMenu.add_command(**optArgCP)
+                        optArg.clear()
+                        optArgCP.clear()
+                else: 
+                    optArg.update({key: value})
+
+
+#######################################################################
+#######################################################################
