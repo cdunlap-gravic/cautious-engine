7f9ff30d69674b49a9c10dee3005cdc6daa1e73c
Jeahnoh <cdunlap.gravic@gmail.com>
2024-07-22 14:36:22 -0400

    added menu building test script

---
+++ menuBuilderScript.py
@@ -0,0 +1,19 @@
+import tkinter as tk 
+from tkinter import ttk
+from ttkthemes import ThemedTk
+
+
+WIN_TITLE = 'Test Script - MenuBar Generation Test 001'
+WIN_SIZE = (800, 600)
+jsonFile = "./genFiles/menu.json"
+root = ThemedTk(theme='black')
+root.title(WIN_TITLE)
+root.geometry(f'{WIN_SIZE[0]}x{WIN_SIZE[1]}')
+
+menuBar = tk.Menu(root)
+root.config(menu=menuBar)
+
+frame = ttk.Frame(root, width=WIN_SIZE[0], height=WIN_SIZE[1])
+frame.pack(fill=tk.BOTH, expand=True)
+
+root.mainloop()
