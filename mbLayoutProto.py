892ec3a945cd588b524c03d308a8bfb690db5af6
Jeahnoh <cdunlap.gravic@gmail.com>
2024-08-01 14:02:44 -0400

    just found a bug, added issue for it. Using this as a breaking point to clean up the directory and rename files appropriately.
    Current progress:
    - just prototype display with hardcoded json file
    - menuObjs have some attributes, needs more development

---
+++ mbLayoutProto.py
@@ -0,0 +1,35 @@
+
+import modules.menuFromJson as MJ
+import tkinter as tk 
+from tkinter import ttk
+from ttkthemes import ThemedTk
+from threading import Thread
+
+import sdl2
+import sdl2.ext
+import sdl2.sdlimage
+import pyboy
+# Global variables
+
+WIN_TITLE = 'Test Script - Menu Builder layout'
+WIN_SIZE = (800, 600)
+jsonFile = "./genFiles/sketch.json"
+root = ThemedTk(theme='black')
+root.title(WIN_TITLE)
+root.geometry(f'{WIN_SIZE[0]}x{WIN_SIZE[1]}')
+
+menuBar = tk.Menu(root)
+root.config(menu=menuBar)
+MJ.loadMenuFromJson(jsonFile, menuBar)
+
+frame = ttk.Frame(root, width=WIN_SIZE[0], height=WIN_SIZE[1])
+frame.pack(fill=tk.BOTH, expand=True)
+#print(MJ.recLoadMenuFromJson.__doc__)
+#print(dir(MJ))
+#print(help(MJ.recLoadMenuFromJson))
+
+t1 = Thread(target = root.mainloop())
+t2 = Thread(target = ...)
+
+t1.start()
+t2.start()
