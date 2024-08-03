be523ccc3d627971082b393347691d1104992fa4
Jeahnoh <cdunlap.gravic@gmail.com>
2024-08-03 12:14:11 -0400

    just added a quick basic window example for MBM.py (also a test commit from CB)

--- modules/menuBuilderMenus.py
+++ modules/menuBuilderMenus.py
@@ -19,3 +19,16 @@ def open_new_window(rootMenu):
     submit_button = tk.Button(new_window, text="Submit", command=submit_text)
     submit_button.pack()
     # Add content to the new window here
+
+
+# Basic window example with a close button
+def baseWindow(rootMenu):
+    new_window = tk.Toplevel(rootMenu)
+    new_window.title("New Window")
+    def closeWindow():
+        new_window.destroy()
+    textLabel = tk.Label(new_window, text="Test window.")
+    textLabel.pack()
+
+    closeButton = tk.Button(new_window, text="Close", command=closeWindow)
+    closeButton.pack()
