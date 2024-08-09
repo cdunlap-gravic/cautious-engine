5d03aad90620df6544774e2e3f0c0b89b630e7e4
Jeahnoh <cdunlap.gravic@gmail.com>
2024-08-09 12:00:05 -0400

    figured it out. precleanup commit want to save these thoughts before I purge and fix a bunch

--- mbObjProto.py
+++ mbObjProto.py
@@ -17,7 +17,7 @@ class MenuObj:
         # through the MenuObj/MenuGroup object relationship
         self.type = type
         self.tags = {k: v for k, v in kwargs.items() if k != 'subMenu'}
-        self.subMenu = kwargs.get('subMenu', None) # this will be a list of dicts
+        self.subMenu = kwargs.get('subMenu', []) # this will be a list of dicts
         # OK BUT, k:v stuff, every item in the submenu should be a menuobj that could hold a menugroup. so submenu IS a menu group?
         # ok but if this is the case, then I should only get one item back and that will be a menugroup object.
         # THIS SUBMENU SHOULD TECHINCALLY BE A MENU GROUP RIGHT?
@@ -35,13 +35,21 @@ class MenuObj:
     def defineUnderlines(self): # sorts out all underlines in the topmost layer. This func should also call itself on specific children objects for each cascade layer; This should also be automatically called whenever changes are made to the whole cascade/menubar
         pass
     
+class MenuObjV2:
+    def __init__(self, type, **kwargs) -> None:
+        self.type = type
+        self.tags = {k: v for k, v in kwargs.items() if k != 'subMenu'}
+        self.subMenu = kwargs.get('subMenu', None) # this will be a LIST[] THAT:S ARELADY FREAKIN ORDERED. JUST FUCKING REARANGE THEM.
+        # techincally subMenu is a list of dicts, not a list of MenuObjs straight from the json, so we will have to figure out how to treat them here I CAN KILL THIS
+        # ACTUALLY, I should probably separate tags from mykeys. If it has a menu name
 
-
-class MenuGroup:
-    def __init__(self, name=None, **kwargs) -> None:
-        self.name = name
+class MenuGroup():
+    def __init__(self, topObj:MenuObj=None, **kwargs) -> None:
+    #def __init__(self, **kwargs): # ITS ALREADY A FUCKING GROUP. IF IT IS OF TYPE MENU IT IS A FUCKING MENUGROUP ALREADY, AND IN TURN, MENUGROUPS ARE ALREADY MENU OBJECTS, THEY JUST HAVE CHILDREN. I PROBABLY DONT NEED A MENUGROUP OBJECT, JUST A FUCKING REORDER ABUILITY
+        self.topObj = topObj
         #need to repeat some of that stuff from MenuObj
-        self.menu_objects = kwargs
+        #NO, it should havea TOP OBJECT, and then the last arg would be a set of menuobjs
+        self.menu_objects = kwargs # 
     def add_menu_object(self, menu_object):
         self.menu_objects.append(menu_object)
 
@@ -55,7 +63,12 @@ class MenuGroup:
 
 
 # I ALSO NEED TO WRITE THESE OBJECTS TO FILE / vars() will be helpful, but I will need to parse them properly
-rootMenu = MenuObj(type='menu',menuName='root', subMenu=[{'type':'menu', 'menuName':'fileMenu'}]) 
-#roo2Menu = MenuObj(type='menu',menuName='root', subMenu=MenuGroup(name='fileMenu', {'type':'menu', 'menuName':'fileMenu'}))
+rootMenu = MenuObj(type='menu',menuName='root', subMenu=[MenuObj(type='menu', menuName='fileMenu')]) 
+roo2Menu = MenuGroup(MenuObj(type='menu',menuName='root',subMenu=MenuGroup(MenuObj)))
 print(vars(rootMenu))
-#print(vars(roo2Menu.subMenu))
+print((roo2Menu.menu_objects))
+
+
+
+
+##### CURRENTLY I AM WORKING ON GETTING A STATICLY/HARDCODED/ STORED FULL MENUBAR TO MAKE SURE EVERYTHING IS STRUCTURED RIGHT
