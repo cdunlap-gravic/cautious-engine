ed59c0c17e7959542b7e60df74f0bfff76cf88b0
Jeahnoh <cdunlap.gravic@gmail.com>
2024-08-09 16:44:53 -0400

    a LOT of progress:
    - learned about frozensets and implemented here
    - added value checks
    - setters and getters
    - raise errors to some to some of those funcs
    - added typehints and return hints

--- mbObjProto.py
+++ mbObjProto.py
@@ -1,55 +1,101 @@
 
 # THIS STUFF IS MORE FOR VALIDATION TESTING BEFORE RUNNING MFJ.PY, BUT CAN ALSO BE USED TO LIST/VALIDATE OPTIONS IN MBMs. This architecture could also be used for the lic builder, listing all the options. I should probably consider  building a configuration set file to load these from, and/or a table
-menu_types = {'menu', 'command', 'iterator', 'separator'}
+my_keys = frozenset({'type', 'menuName', 'subMenu'})
+menu_types = frozenset({'menu', 'command', 'iterator', 'separator'})
+tkinter_args = frozenset({'accelerator', 'activebackground', 'activeforeground', 'background', 'bitmap', 'columnbreak', 'command', 'compound', 'font', 'foreground', 'hidemargin', 'image', 'label', 'menu', 'offvalue', 'onvalue', 'selectcolor', 'selectimage', 'state', 'underline', 'value', 'variable'})
 
-tkinter_args = {'accelerator', 'activebackground', 'activeforeground', 'background', 'bitmap', 'columnbreak', 'command', 'compound', 'font', 'foreground', 'hidemargin', 'image', 'label', 'menu', 'offvalue', 'onvalue', 'selectcolor', 'selectimage', 'state', 'underline', 'value', 'variable'}
+currently_known_not_supported = frozenset({'font'})
+needs_functions_to_be_a_useful_tag = frozenset({'accelerator'})
 
-my_keys = {'type', 'menuName', 'subMenu'}
-
-currently_known_not_supported = {'font'}
+allowed_keys = frozenset(tkinter_args | my_keys)
 # I probably should get these from a written file
 
 
-# I WILL need a way to validate that the KEYS in menu.json either match tkinter_args OR matches my KEYS
+## IN THE MENU EDITOR WINDOWS I WILL NEED TO MAKE A SECOND INSTANCE OF THE MENUOBJ AS A BUFFER BEFORE SAVING THE CHANGE, THAT WAY IF YOU
+## CANCEL, NOTHING IS OVERWRITTEN. ACTUALLY I'LL NEED 2: Actual MenuBar, Cached Version, Current Buffer, IF APPLY, THEN WRITE TO Actual MenuBar
+# I'll also need to somehow convert these objects into my json objects, and vice versa.
 class MenuObj:
-    def __init__(self, type, **kwargs) -> None:
-        # I'll also need to somehow convert these objects into my json objects, and vice versa. NOTE working on that
-        # through the MenuObj/MenuGroup object relationship
+    def __init__(self, type:str, **kwargs:dict) -> None:
+        if type not in menu_types:
+            raise ValueError(f"""Invalid type: {type}. Must be one of: {menu_types}""")
         self.type = type
-        self.tags = {k: v for k, v in kwargs.items() if k != 'subMenu'}
-        self.subMenu = kwargs.get('subMenu', []) # this will be a list of dicts
-        # OK BUT, k:v stuff, every item in the submenu should be a menuobj that could hold a menugroup. so submenu IS a menu group?
-        # ok but if this is the case, then I should only get one item back and that will be a menugroup object.
-        # THIS SUBMENU SHOULD TECHINCALLY BE A MENU GROUP RIGHT?
+        
+        if invalid_keys(kwargs):
+            raise ValueError(f"""Invalid keys: {invalid_keys(kwargs)}""")
+        
+        self.tags = {k: v for k, v in kwargs.items() if k not in my_keys}
+        self.menuName = kwargs.get('menuName', None)
+        self.subMenu = kwargs.get('subMenu', [])
+        
+        #OK SO BASE OBJECT IS MADE. Once things are added to it, we need to be running a check on the memebers of a submenu if there is one
+        # /and there certainly is one in root and it's first child/ and we need to run that check WHEN members are added. 
+        
+    #setters
+    
+    def set_type(self, type:str) -> None:
+        if type not in menu_types:
+            raise ValueError(f"""Invalid type: {type}. Must be one of: {menu_types}""")
+        self.type = type
+        
+    def set_tags(self, **kwargs:dict) -> None:
+        if invalid_keys(kwargs):
+            raise ValueError(f"""Invalid keys: {invalid_keys(kwargs)}""")
+        self.tags = {k: v for k, v in kwargs.items() if k not in my_keys}
+    
+    #Should I even set the menuName without validating if it already has one? This really shouldn't be called without verifying ¯\_(ツ)_/¯
+    def set_menuName(self, menuName:str) -> None:
+        self.menuName = menuName
+        
+    #getters
+    
+    def get_type(self) -> str:
+        return self.type
+    
+    #TODO should probably validate that the tag ISNT MYKEY
+    def get_tag(self, tag:str) -> any:
+        try:
+            return self.tags.get(tag)
+        except KeyError:
+            raise KeyError(f"""Tag '{tag}' not found in MenuObj""")
+    
+    def get_menuName(self) -> str:
+        try:
+            return self.menuName
+        except AttributeError:
+            raise AttributeError(f"""MenuObj does not have a menuName""")
+    
+    #type functions if I need any...
+    
+    #TODO ...
+    
+    #Tag functions which I will need
+    
+    #TODO ...
+    
+    # submenu functions 
+    
+    #TODO ....    
+        
+        
+        # ALL OBJECTS IN SUBMENU WILL BE A LIST OF MENUOBJS. SOME LISTS MAY ONLY HAVE 1 MEMBER AND THATS OK, BUT OTHERS WILL BE A LIST OF MENUOBJS THAT ONLY HAVE SUBMENUS(other lists) and THATS OK TOO! We'll be INSERTING another MenuObj bewteen MenuObjs that have self.type == 'menu'
+        
+    #insert separator
+    """_'psuedo'_
+
+    def addSeparator(self):
+        for i in self.subMenu.items():
         
     
-    #TODO: SETTERS
-    def set_tag(self, tag: dict): #expecting single dict item, needs verification that it is an accepted tkinter_args, or my_keys
-        pass
+    """
+
     
-    #TODO: GETTERS
-    def get_tag(self, tag: str): # returns value of a specific tag, OR, return that the tag has not yet been set (not found)
-        pass
+    
     
     #TODO: Other Funcs
     def defineUnderlines(self): # sorts out all underlines in the topmost layer. This func should also call itself on specific children objects for each cascade layer; This should also be automatically called whenever changes are made to the whole cascade/menubar
         pass
-    
-class MenuObjV2:
-    def __init__(self, type, **kwargs) -> None:
-        self.type = type
-        self.tags = {k: v for k, v in kwargs.items() if k != 'subMenu'}
-        self.subMenu = kwargs.get('subMenu', None) # this will be a LIST[] THAT:S ARELADY FREAKIN ORDERED. JUST FUCKING REARANGE THEM.
-        # techincally subMenu is a list of dicts, not a list of MenuObjs straight from the json, so we will have to figure out how to treat them here I CAN KILL THIS
-        # ACTUALLY, I should probably separate tags from mykeys. If it has a menu name
 
-class MenuGroup():
-    def __init__(self, topObj:MenuObj=None, **kwargs) -> None:
-    #def __init__(self, **kwargs): # ITS ALREADY A FUCKING GROUP. IF IT IS OF TYPE MENU IT IS A FUCKING MENUGROUP ALREADY, AND IN TURN, MENUGROUPS ARE ALREADY MENU OBJECTS, THEY JUST HAVE CHILDREN. I PROBABLY DONT NEED A MENUGROUP OBJECT, JUST A FUCKING REORDER ABUILITY
-        self.topObj = topObj
-        #need to repeat some of that stuff from MenuObj
-        #NO, it should havea TOP OBJECT, and then the last arg would be a set of menuobjs
-        self.menu_objects = kwargs # 
+    #TODO SUBMENU FUNCTIONS
     def add_menu_object(self, menu_object):
         self.menu_objects.append(menu_object)
 
@@ -59,16 +105,10 @@ class MenuGroup():
     
     #TODO rename objects functions
     
-# Do I actually currently have support for a full menubar and all its parameters yet? Let's test it?
+    
+def invalid_keys(**kwargs:dict) -> set:
+    invalid_keys = set(kwargs.keys()) - allowed_keys
+    return invalid_keys
 
 
-# I ALSO NEED TO WRITE THESE OBJECTS TO FILE / vars() will be helpful, but I will need to parse them properly
-rootMenu = MenuObj(type='menu',menuName='root', subMenu=[MenuObj(type='menu', menuName='fileMenu')]) 
-roo2Menu = MenuGroup(MenuObj(type='menu',menuName='root',subMenu=MenuGroup(MenuObj)))
-print(vars(rootMenu))
-print((roo2Menu.menu_objects))
-
-
-
-
-##### CURRENTLY I AM WORKING ON GETTING A STATICLY/HARDCODED/ STORED FULL MENUBAR TO MAKE SURE EVERYTHING IS STRUCTURED RIGHT
+print('test')
