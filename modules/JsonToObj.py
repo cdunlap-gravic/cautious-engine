2a3cf1b65390cbd97d4ce63181f58953e81d4e73
Jeahnoh <cdunlap.gravic@gmail.com>
2024-08-23 16:32:46 -0400

    started this and hit a brick wall. Just a case of writer's block. A long weekend should find me the solution

--- modules/JsonToObj.py
+++ modules/JsonToObj.py
@@ -4,5 +4,15 @@ from objects.MenuItem import *
 
 from hcRootMI import *
 
-
 print(rootTest1.menuName)
+
+jsonFile = 'output.json'
+
+with open(jsonFile) as file:
+    json = file.read()
+    
+print(json)
+
+
+# ! now what to do with <json>
+
